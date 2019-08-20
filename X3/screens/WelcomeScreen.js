8553e5190ba5bde71920f1578091a96fb5ae66d9
GeorgeCast <georgecast12@gmail.com>
2019-08-19 18:47:27 -0700

    fontUpdate

--- X3/screens/WelcomeScreen.js
+++ X3/screens/WelcomeScreen.js
@@ -1,35 +1,35 @@
 import React from "react";
 import {
-	Image,
-	Platform,
-	ScrollView,
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	View,
-	ImageBackground,
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ImageBackground
 } from "react-native";
 
 export default function WelcomeScreen() {
-	return (
-		<ImageBackground
-			source={require("../assets/images/sailboat.jpeg")}
-			style={{ width: "100%", height: "100%" }}
-		>
-			<Text style={styles.welcomeText}>X-3OATS</Text>
-		</ImageBackground>
-	);
+  return (
+    <ImageBackground
+      source={require("../assets/images/sailboat.jpeg")}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <Text style={styles.welcomeText}>X-3OATS</Text>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-	welcomeText: {
-		marginBottom: 20,
-		marginTop: 175,
-		color: "#DAA520",
-		fontSize: 75,
-		fontWeight: "bold",
-		textAlign: "center",
-		fontFamily: "monospace",
-		opacity: 0.8,
-	},
+  welcomeText: {
+    marginBottom: 20,
+    marginTop: 175,
+    color: "#DAA520",
+    fontSize: 75,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "Arial",
+    opacity: 0.8
+  }
 });
